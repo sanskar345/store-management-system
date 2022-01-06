07018daaf58d902a182be55728739d63aef8458b
Sanskar Soni <sonisanskar223@gmail.com>
2022-01-06 13:23:26 +0530

    added changePictureDialog

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ChangePictureDialogComponent } from 'src/app/dialogs/change-picture-dialog/change-picture-dialog.component';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  openAddItemCategoryDialog() {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    let dialog = this.dialog.open(ChangePictureDialogComponent, {
+      data : {
+      }
+    });
   }
 
 }
